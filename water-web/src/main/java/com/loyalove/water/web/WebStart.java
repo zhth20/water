d92b89f6ad0b5a4c0b646474d74bf374e380d645
loyalove <598733638@qq.com>
2017-01-03 12:58:25 +0800

    修复main函数启动报错问题

--- water-web/src/main/java/com/loyalove/water/web/WebStart.java
+++ water-web/src/main/java/com/loyalove/water/web/WebStart.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Title: WebApiStart.java
@@ -27,7 +27,7 @@ public class WebStart extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(WebStart.class, args);
-        logger.info("Web服务启动成功");
+        EmbeddedWebApplicationContext context = (EmbeddedWebApplicationContext) SpringApplication.run(WebStart.class, args);
+        logger.info("Web服务启动成功，访问：http://localhost:{}",context.getEmbeddedServletContainer().getPort());
     }
 }
