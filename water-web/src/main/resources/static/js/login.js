dc19fd9440f5db67f975ac1c07f4d308a7be1746
loyalove <598733638@qq.com>
2017-01-16 21:40:38 +0800

    添加登录页面特效

--- water-web/src/main/resources/static/js/login.js
+++ water-web/src/main/resources/static/js/login.js
@@ -1,15 +1,17 @@
-/**
- * Created by Loyal on 2016/12/9.
- */
+layui.config({
+    base: '/static/js/common/'
+});
+
 if(!(window.top === window.self)){
     window.parent.location.reload();
 }
-layui.use(['element', 'form'], function () {
+layui.use(['element', 'form', 'article'], function () {
     //导航的hover效果、二级菜单等功能，需要依赖element模块
     var $ = layui.jquery,
         element = layui.element(),
         form = layui.form(),
         layer = layui.layer;
+    layui.article($);
     $('.beg-login-box').fadeIn();
     form.on('submit(login)', function (data) {
         $.post('/login', data.field, function (data) {
@@ -21,4 +23,12 @@ layui.use(['element', 'form'], function () {
         return false;
     });
 
+    $(".layui-canvs").jParticle({
+        background: "#000",
+        color: "#009688"
+    });
+
+    $(window).on('resize', function(){
+        $('.layui-canvs').width($(window).width()).height($(window).height());
+    });
 });
